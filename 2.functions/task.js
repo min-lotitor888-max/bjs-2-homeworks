31c44656e9099abbba00db5b093da18fa26bfbb1
serg_ya <lotitor888@gmail.com>
2026-05-28 21:51:52 +0300

    task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,38 @@
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
+	if (arr.length === 0) {
+		return {
+			min: 0,
+			max: 0,
+			avg: 0
+		};
+	}
+
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		const num = arr[i];
+
+		if (num > max) {
+			max = num;
+		}
+
+		if (num < min) {
+			min = num;
+		}
+
+		sum += num;
+	}
+
+	const avg = Number((sum / arr.length).toFixed(2));
+
+	return {
+		min,
+		max,
+		avg
+	};
 }
 
 function summElementsWorker(...arr) {
