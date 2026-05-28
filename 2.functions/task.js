dedcfff7ed2b120e05a940336df4bd9fb943cfbb
serg_ya <lotitor888@gmail.com>
2026-05-28 21:54:22 +0300

    Add task 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -36,19 +36,49 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+	if (arr.length === 0) return 0;
+	return arr.reduce((sum, num) => sum + num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) return 0;
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+	return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+	if (arr.length === 0) return 0;
 
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+
+	for (const num of arr) {
+		if (num % 2 === 0) {
+			sumEvenElement += num;
+		} else {
+			sumOddElement += num;
+		}
+	}
+
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (arr.length === 0) return 0;
 
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+
+	for (const num of arr) {
+		if (num % 2 === 0) {
+			sumEvenElement += num;
+			countEvenElement++;
+		}
+	}
+
+	if (countEvenElement === 0) return 0;
+	return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
